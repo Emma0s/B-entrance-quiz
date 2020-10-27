b5f95354193a05790bf332cbf95ba501327c28c3
wenwei.song <wenwei.song@rea-group.com>
2020-10-27 19:53:09 +0800

    [wenwei] init: connect with frontend

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/GtbEntranceQuizApplication.java
@@ -2,12 +2,22 @@ package com.thoughtworks.capability.gtb.entrancequiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
 @SpringBootApplication
 public class GtbEntranceQuizApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GtbEntranceQuizApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
 	}
 
 }
